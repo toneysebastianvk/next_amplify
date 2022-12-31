54ef43012a4cc8dc16ef84aab2f7efc76e1609b9
Toney Sebastian <toney.sebastian@petco.com>
2022-12-31 12:04:00 +0530

    fetching username from github api

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
-const Index = () => {
-  return <h1>your sample next application</h1>;
+import axios from "axios";
+
+const Index = ({userData}) => {
+  return (
+    <div>
+      <h1>your sample next application</h1>
+      <h2>Welcome git lab user, {userData.name}</h2>
+    </div>
+  );
 };
+
+export async function getServerSideProps(context) {
+  const userData = await getUserData();
+  console.log("userDatauserDatauserData= ", userData.data);
+  return {
+    props: {
+      userData: userData.data,
+    },
+  };
+}
+
+async function getUserData() {
+  return axios.get("https://api.github.com/users/toneysebastianvk");
+}
 
 export default Index;
